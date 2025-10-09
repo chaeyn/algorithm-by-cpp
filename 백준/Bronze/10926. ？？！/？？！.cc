a22338050ec7c86dfa4685b56da7e9a857c9dd76
채근영 <chaulfe@gmail.com>
2025-10-09 16:16:39 +0900

    [Bronze V] Title: ??!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 빠른 입출력
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    string id;
+    cin >> id;
+    cout << id << "??!\n";
+
+    return 0;
+}
