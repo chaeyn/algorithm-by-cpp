92ef082b68329737c51e8324e99abf292db16a8b
채근영 <chaulfe@gmail.com>
2025-10-09 18:08:49 +0900

    [Bronze IV] Title: 주사위 세개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    if (a == b && b == c) cout << 10000 + a * 1000;
+    else if (a == b || a == c) cout << 1000 + a * 100;
+    else if (b == c) cout << 1000 + b * 100;
+    else cout << max(a, (max(b, c))) * 100;
+
+    return 0;
+}
