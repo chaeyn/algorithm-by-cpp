92cb7cede6ba3ef0c4bea0012e8676bb93a36a97
채근영 <chaulfe@gmail.com>
2025-10-22 07:45:18 +0900

    [Silver V] Title: 카드1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2161. 카드1/카드1.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    queue<int> q;
+    for (int i=1; i<=n; i++) {
+        q.push(i);
+    }
+
+    while (q.size() > 0) {
+        cout << q.front() << " ";
+        q.pop();
+        int pop = q.front();
+        q.push(pop);
+        q.pop();
+   }
+
+    return 0;
+}
